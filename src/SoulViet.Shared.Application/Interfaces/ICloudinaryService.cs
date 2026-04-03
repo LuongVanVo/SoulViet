33b2db394625f10affd145650561035e3a24b324
vanvo299 <luongvanvo29@gmail.com>
2026-04-04 00:38:14 +0700

    feat(cloudinary): implement Cloudinary service for image upload and deletion

---
+++ src/SoulViet.Shared.Application/Interfaces/ICloudinaryService.cs
@@ -0,0 +1,10 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace SoulViet.Shared.Application.Interfaces;
+
+public interface ICloudinaryService
+{
+    Task<string> UploadImageAsync(IFormFile file, string folderName);
+
+    Task<bool> DeleteImageAsync(string publicId);
+}
